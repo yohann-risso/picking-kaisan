09269d561436ebe08d9ec0646516227c60856eba
yohann-risso <yohannrisso@gmail.com>
2025-05-23 13:39:11 -0300

    update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Supabase config
-const SUPABASE_URL = "https://kinpwzuobsmfkjefnrdc.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpbnB3enVvYnNtZmtqZWZucmRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5OTgwMjcsImV4cCI6MjA2MzU3NDAyN30.btmwaLMSnXCmvKHQvYnw7ZngONqoejqnhbvazLhD1Io";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 
 let produtos = [], retirados = [], tempoInicio = null, cronometroInterval = null;
 
